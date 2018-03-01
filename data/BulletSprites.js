3db118b7375d71972a4063b84425c3bf950e56b8
brycemcconnell <brycemcconnell@gmail.com>
2018-03-01 15:58:04 +1100

    TODO: Bullet damage

--- data/BulletSprites.js
+++ data/BulletSprites.js
@@ -1,6 +1,14 @@
 export const BulletSprites = {
 	"Basic": {
   	  sprite: "sprites/laser.png",
+  	  damage: 1,
   	  speed: 1.5
+  	},
+  	// Implement damage system
+  	"Heavy": {
+  		sprite: "...",
+  		damage: 2,
+  		speed: 1
   	}
+
 };
